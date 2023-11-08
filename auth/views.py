0e53e84e9f7de17db6e261c11318a704c1732df7
shonpersus <shonpersus07@gmail.com>
2023-11-08 16:18:38 +0300

    first commit

---
+++ auth/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User registered successfully')
+            return redirect('registration-url')
+    else:
+        form = RegistrationForm()
+    return render(request, 'auth/register.html', {"form": form})
+
+
+def login(request):
+    return render(request, 'auth/login.html')
